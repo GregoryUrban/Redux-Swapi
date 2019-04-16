1add94c075f8c2868c1205f7089efe68efc2bc4b
GregoryUrban <turknben@gmail.com>
2019-04-16 13:46:37 -0400

    initial commit

--- src/index.js
+++ src/index.js
@@ -3,18 +3,17 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { Provider } from "react-redux";
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
 import rootReducer from "./reducers";
+import thunk from "redux-thunk";
+import logger from 'redux-logger';
 // needed dependancies
-// applyMiddleware from redux
-// thunk from redux-thunk
-// logger from redux-logger
-// rootReducer from ./reducers
+// applyMiddleware from redux DONE above
+// thunk from redux-thunk DONE above
+// logger from redux-logger  DONE below
+// rootReducer from ./reducers - already DONE?
 
-const store = createStore(
-  rootReducer
-  /* applyMiddleware goes here */
-);
+const store = createStore(rootReducer, applyMiddleware(thunk, logger));
 
 ReactDOM.render(
   <Provider store={store}>
