7c65ed59719fb04c08333ea63f9bc434ca43e100
GregoryUrban <turknben@gmail.com>
2019-04-16 22:45:29 -0400

    Added gender, dob, more styling

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Character = props => {
-  return <li>{props.character.name}</li>;
+  return <div>
+  <h3>{props.character.name} </h3>
+  <p>Gender: {props.character.gender}</p> 
+  <p>dob: {props.character.birth_year}</p>
+  </div>;
 };
 
 export default Character;
